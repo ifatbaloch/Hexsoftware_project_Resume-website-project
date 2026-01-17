81ec5c545a212ff52276d76076236da903321719
ifat aziz <ifatbaloch5@gmail.com>
2026-01-17 19:27:08 +0500

    Add files via upload

---
+++ resume/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    console.log("Portfolio loaded successfully");
+};
+
+// Simple alert on page load (optional)
+setTimeout(function () {
+    alert("Welcome to my portfolio website!");
+}, 1000);
